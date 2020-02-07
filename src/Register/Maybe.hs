fee070683864c799f6aa523624d589aafe4183c2
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-02-07 15:13:29 +0000

    Add Maybe instruction for splitting into two paths

---
+++ src/Register/Maybe.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Register.Maybe where
+
+import Prelude hiding (Maybe)
+import qualified Control.Lens as L
+import Control.Lens ((^.), (.~), (%~))
+import Register
+import Text.ParserCombinators.ReadP
+import Data.Functor.Classes
+
+data Maybe label = Maybe label
+
+instance InstructionF [] Maybe a where
+    interpretF (Maybe label) machinestate
+      = [ L.over position (fmap succ) machinestate
+        , L.set position label machinestate
+        ]
+
+-- Parse in a regular instruction
+parseInstr :: ReadP label -> ReadP (Maybe label)
+parseInstr label = do
+    string "maybe"
+    sp
+    label <- label
+    pure $ Maybe label
+
+instance Read1 Maybe where
+    liftReadsPrec readsPrec _ _ = readP_to_S $ parseInstr (readS_to_P (readsPrec 10))
+
