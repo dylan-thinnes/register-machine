53c908680668a6b6cde2ed9fd7ec127df8897ab2
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-02-10 11:00:53 +0000

    Add -t trace flag

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,26 @@ module Main where
 import Register
 import Register.Instr
 
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
+
 -- Read a machine from stdin, run it, then report its registers
 main :: IO ()
-main = fmap read getContents >>= runCW
+main = do
+    args <- getArgs
+    program <- read <$> getContents
+    if "-t" `elem` args
+       then runTrace program
+       else runCW program
 
 -- Run machine and print according to coursework spec
 runCW :: Machine Label -> IO ()
 runCW machine = putStrLn $ reportRegisters $ _registers $ _state $ runEnd machine
+
+-- Run with full debug
+runTrace :: Machine Label -> IO ()
+runTrace machine = do
+    let trace = run machine
+    mapM_ (hPutStrLn stderr . debug True) trace
+    putStrLn $ reportRegisters $ _registers $ _state $ last trace
+
