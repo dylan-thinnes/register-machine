c532de1cbbc4d14ebcf91e12be9e7edbcb10b263
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-02-12 11:55:30 +0000

    Add interactive interpreter for machine

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Main where
 
 import Register
 import Register.Instr
+import Register.Maybe
 
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
+
+import Data.Functor.Compose
+import Control.Comonad.Trans.Cofree
+import Data.Foldable
+import Safe (atMay, readMay)
+import Data.Default
 
 -- Read a machine from stdin, run it, then report its registers
 main :: IO ()
@@ -30,3 +41,27 @@ runTrace machine = do
     mapM_ (hPutStrLn stderr . debug True) trace
     reportRegistersIO $ last trace
 
+-- Run interactively, giving the user the choice of movement down the tree
+runInteractive :: forall f label instr value. (InstructionF f instr value, Foldable f, Applicative f, Functor instr, Default value, Eq label, Show value, Show label, Show (instr String))
+               => GMachine label instr value
+               -> IO ()
+runInteractive machine = do
+    finalState <- interact $ runF machine
+    reportRegistersIO finalState
+    where
+        interact :: MachineTrace f (GMachine label instr value) -> IO (GMachine label instr value)
+        interact (runCofree -> m :< rest) = do
+            hPutStrLn stderr $ debug True m
+            case getCompose rest of
+              Just rest -> chooseBranch rest
+              Nothing   -> hPutStrLn stderr "Machine halted." >> pure m
+        chooseBranch f = prompt (toList f) >>= interact
+        prompt choices = do
+            hPutStrLn stderr $ "There are " ++ show (length choices) ++ " option(s)."
+            result <- tryParseInput choices <$> getLine
+            case result of
+              Just res -> pure res
+              Nothing  -> hPutStrLn stderr "Invalid input. Try again." >> prompt choices
+        tryParseInput choices s = do
+            ii <- readMay s
+            choices `atMay` ii
