d51eb570837080c15ba5120940c994bf36abb2f1
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-02-12 11:42:33 +0000

    Refactor out "reportRegistersIO" function for Main

--- app/Main.hs
+++ app/Main.hs
@@ -15,14 +15,18 @@ main = do
        then runTrace program
        else runCW program
 
+-- Report the registers to IO given a final machine
+reportRegistersIO :: (Show value, Default value) => GMachine label instr value -> IO ()
+reportRegistersIO = putStrLn . reportRegisters . _registers . _state
+
 -- Run machine and print according to coursework spec
 runCW :: Machine Label -> IO ()
-runCW machine = putStrLn $ reportRegisters $ _registers $ _state $ runEnd machine
+runCW machine = reportRegistersIO $ runEnd machine
 
 -- Run with full debug
 runTrace :: Machine Label -> IO ()
 runTrace machine = do
     let trace = run machine
     mapM_ (hPutStrLn stderr . debug True) trace
-    putStrLn $ reportRegisters $ _registers $ _state $ last trace
+    reportRegistersIO $ last trace
 
